3809e8779beee63c7dc193b5a1e14c09841b9381
Jeefry <frisk7530@gmail.com>
2024-11-11 09:59:23 -0500

    Create Structure Project

---
+++ app/src/main/java/com/puyodev/xlab13animations/MainActivity.kt
@@ -0,0 +1,58 @@
+package com.puyodev.xlab13animations
+
+import FormularioAnimado
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.puyodev.xlab13animations.ui.theme.XLab13AnimationsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            XLab13AnimationsTheme {
+
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(20.dp,50.dp),
+                        ) {
+                            FormularioAnimado()
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    XLab13AnimationsTheme {
+        Greeting("Android")
+    }
+}
